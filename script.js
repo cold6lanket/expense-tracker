946ac47dc51e7c61b2d3b0c0b58d20598e33befa
Feiertag <example@gmail.com>
2021-04-16 23:14:10 +0500

    chart added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // retrive dom elements
+
 // input elements
 const transactionText = document.querySelector('#text');
 const transactionAmount = document.querySelector('#amount');
@@ -111,3 +112,34 @@ function deleteItem(e) {
     }
     
 }
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const data = {
+    labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+    ],
+    datasets: [{
+        label: 'My First Dataset',
+        data: [300, 50, 100],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+    }],
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+};
+
+const config = {
+    type: 'pie',
+    data: data,
+};
+
+const chart = new Chart(ctx, config);
